77928933248170ec875f5dc22d26bc9f3393512e
Sastermin <dar007alex@gmail.com>
2026-03-17 17:33:12 -0600

    fix: corregir fallos en tests de CI

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,9 +19,9 @@ return new class extends Migration
             $table->string('password');
 
             //Nuevos campos
-            $table->string('id_number')->unique();
-            $table->string('phone');
-            $table->string('address');  
+            $table->string('id_number')->nullable()->unique();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
